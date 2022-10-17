6fe28b5cfe14162dd570b8b969f81f9099fd0b35
est22 <anlia.seoul@gmail.com>
2022-10-17 20:22:39 +0900

    2d array

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+    ["4 + 5 = 9", "True"],
+    ["6 * 4 = 24", "True"],
+    ["6 - 9 = -5", "False"]
+    ]
+    
+    
+    var questionNumber = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle // True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        
+        updateUI()
+        
+        if userAnswer == actualAnswer {
+            print("right!")
+        } else {
+            print("wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+    }
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 
 }
 
